5eae2dfb229dc6d05989fd97b6658b0adc97e5ec
ahmedmosaad19999 <ahmed.mosaad@mahaseel.net>
2025-08-10 15:45:39 +0300

    update the route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
-const Homepage = () => {
-  return (
-    <div className=''>Homepage</div>
-  )
-}
+"use client";
 
-export default Homepage
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+const Homepage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace("/admin");
+  }, [router]);
+
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="text-lg">Redirecting to Admin Dashboard...</div>
+    </div>
+  );
+};
+
+export default Homepage;
